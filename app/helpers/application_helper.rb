f3ddd16f0f5a2e77052cac94c43b820c49646288
Scott Luptowski <scottluptowski@gmail.com>
2016-07-08 13:10:00 -0400

    fix times

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def lottery_start
-    DateTime.now.midnight.in_time_zone('Eastern Time (US & Canada)') + 9.hours
+    DateTime.now.in_time_zone('Eastern Time (US & Canada)').midnight + 9.hours
   end
 
   def lottery_end
-    DateTime.now.midnight.in_time_zone('Eastern Time (US & Canada)') + 16.hours
+    DateTime.now.in_time_zone('Eastern Time (US & Canada)').midnight + 16.hours
   end
 
   def time_now
